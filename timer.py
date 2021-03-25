ee8f29fcb4a1d28c0d889babbe272fa96e8f5abe
ilyas mutlu <79372941+ilyaasmutlu@users.noreply.github.com>
2021-03-25 20:46:00 +0300

    Create timer.py

---
+++ timer.py
@@ -0,0 +1,36 @@
+# coding=utf-8
+
+"""
+    Timer usage A small app that displays date
+"""
+
+import time
+import os
+import platform
+
+
+def clear():
+    if platform.system() == 'Linux' or platform.system() == 'Darwin':
+        os.system('clear')
+    else:
+        os.system('cls')
+
+
+while True:
+
+    timer = time.localtime()
+    year = timer[0]
+    mount = timer[1]
+    day = timer[2]
+    hour = timer[3]
+    minute = timer[4]
+    second = timer[5]
+
+    time.sleep(1)
+    clear()
+
+    print("""
+        tarih: {}/{}/{}
+        saat : {}:{}:{}
+        """.format(day, mount, year, hour, minute, second))
+        

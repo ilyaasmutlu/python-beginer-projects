c41cb6457279c81f73bf1e1d7808768d1d245f1f
ilyas mutlu <79372941+ilyaasmutlu@users.noreply.github.com>
2021-03-29 20:57:27 +0300

    Create web_search.py

---
+++ web_search.py
@@ -0,0 +1,10 @@
+import random
+import time
+import  webbrowser
+
+while True:
+    sites = random.choice(['google.com','facebook.com'])
+    visit= "https://{}".format(sites)
+    webbrowser.open(visit)
+    seconds=random.randint(2,5)
+    time.sleep(seconds)
